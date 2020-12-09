ad56f308ce50a931d2849348fd0bc28740f8545d
Rolf Rosenbaum <rolf.rosenbaum@rewe-digital.com>
2020-12-09 08:17:49 +0100

    Day 9 solution, List.windowed() FTW!!

--- src/main/kotlin/day9/Day9.kt
+++ src/main/kotlin/day9/Day9.kt
@@ -12,10 +12,39 @@ fun main() {
 }
 
 private fun readInputData() =
-    {}.javaClass.classLoader.getResourceAsStream("day7.txt")
+    {}.javaClass.classLoader.getResourceAsStream("day9.txt")
         .reader()
         .readLines()
+        .map(String::toLong)
 
-fun partOne(input: List<String>): Int = 0
+fun partOne(input: List<Long>): Long {
+    for (index in 25 until input.size) {
+        val candidate = input[index]
+        val subList = input.subList(index - 25, index)
+        var found = false
+        for (i in subList.indices) {
+            for (j in subList.indices) {
+                if (subList[i] + subList[j] == candidate && subList[i] != subList[j]) {
+                    found = true
+                    break
+                }
+            }
+        }
+        if (!found) return candidate
+    }
+    return -1
+}
 
-fun partTwo(input: List<String>) = 0
+fun partTwo(input: List<Long>): Long {
+
+    val candidate: Long = partOne(input)
+
+    for (window in 2 until input.size) {
+        val index = input.windowed(window).indexOfFirst { it.sum() == candidate }
+        if (index != -1) {
+            val list = input.windowed(window)[index]
+            return list.minOf { it } + list.maxOf { it }
+        }
+    }
+    return -1
+}
