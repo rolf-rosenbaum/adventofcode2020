4b2a89174300b8fdd4e74c955e645b7587caa0aa
Rolf Rosenbaum <rolf.rosenbaum@rewe-digital.com>
2020-12-17 10:03:39 +0100

    Day 17 solution part1

--- src/main/kotlin/day17/Day17.kt
+++ src/main/kotlin/day17/Day17.kt
@@ -3,19 +3,106 @@ package day17
 fun main() {
 
     val input = readInputData()
+    val source = initSource(input)
 
-    val result1 = partOne(input)
+    val result1 = partOne(source)
     println("Result1: $result1")
 
-    val result2 = partTwo(input)
+    val result2 = partTwo(source)
     println("Result2: $result2")
 }
 
-private fun readInputData() =
+typealias EnergySource = Set<Cell>
+
+private fun initSource(lines: List<String>): EnergySource {
+    val result = mutableSetOf<Cell>()
+    lines.forEachIndexed { y, line ->
+        line.forEachIndexed { x, c ->
+            if (c == '#')
+                result.add(Cell(x, y, 0))
+        }
+    }
+    return result
+}
+
+private fun EnergySource.countNeighbors(cell: Cell): Int {
+    return count { it.isNeighbor(cell) }
+}
+var cycle: Int = 0
+fun EnergySource.next(): EnergySource {
+    println("Generation: $cycle")
+    prettyPrint()
+    cycle++
+    val next = mutableSetOf<Cell>()
+    return filter { countNeighbors(it) in 2..3 }.toSet() + populateDeadCells()
+}
+
+
+private fun EnergySource.populateDeadCells(): Set<Cell> {
+    val newCells = mutableSetOf<Cell>()
+    newCells.addAll(findAllDeadNeighbors().filter { countNeighbors(it) == 3 })
+    return newCells
+}
+
+private fun EnergySource.findAllDeadNeighbors(): Set<Cell> {
+    val candidates = mutableSetOf<Cell>()
+
+    forEach { cell ->
+        candidates.addAll(findDeadNeighbors(cell))
+    }
+    return candidates
+}
+
+private fun EnergySource.findDeadNeighbors(cell: Cell): Set<Cell> {
+    val candidates = mutableSetOf<Cell>()
+    (cell.x - 1..cell.x + 1).forEach { x ->
+        (cell.y - 1..cell.y + 1).forEach { y ->
+            (cell.z - 1..cell.z + 1).forEach { z ->
+                if (!contains(Cell(x, y, z))) {
+                    candidates.add(Cell(x, y, z))
+                }
+            }
+        }
+    }
+    return candidates
+}
+
+private fun EnergySource.prettyPrint() {
+    val minLayer = minByOrNull{it.z}?.z ?: 0
+    val minX = minByOrNull{it.x}?.x ?: 0
+    val minY = minByOrNull{it.y}?.y ?: 0
+    val maxLayer = maxByOrNull{it.z}?.z ?: 0
+    val maxX = maxByOrNull{it.x}?.x ?: 0
+    val maxY = maxByOrNull{it.y}?.y ?: 0
+
+    for (layer in minLayer..maxLayer) {
+        println("Layer: $layer")
+        for (y in minY..maxY) {
+            var line = ""
+            for (x in minX..maxX) {
+                line += if (contains(Cell(x, y, layer))) "#" else "."
+            }
+            println(line)
+        }
+        println()
+    }
+}
+
+
+private fun readInputData(): List<String> =
     {}.javaClass.classLoader.getResourceAsStream("day17.txt")
         .reader()
         .readLines()
 
-fun partOne(input: List<String>): Int = 0
+fun partOne(source: Set<Cell>): Int = generateSequence(source) { it.next() }.drop(6).first().count()
 
-fun partTwo(input: List<String>): Int = 0
+fun partTwo(input: Set<Cell>): Int = 0
+
+data class Cell(val x: Int, val y: Int, val z: Int) {
+    fun isNeighbor(other: Cell): Boolean {
+        return this != other
+                && other.x in x - 1..x + 1
+                && other.y in y - 1..y + 1
+                && other.z in z - 1..z + 1
+    }
+}
