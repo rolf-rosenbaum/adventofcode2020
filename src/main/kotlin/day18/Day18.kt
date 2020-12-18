8a333a9515092f2f87708377a0bfcf67a93f24fb
Rolf Rosenbaum <rolf.rosenbaum@rewe-digital.com>
2020-12-18 22:10:18 +0100

    Day 18 solution

--- src/main/kotlin/day18/Day18.kt
+++ src/main/kotlin/day18/Day18.kt
@@ -11,11 +11,70 @@ fun main() {
     println("Result2: $result2")
 }
 
+fun partOne(input: List<String>): Long =
+    input.map { calculateExpression(it) }.sum()
+
+fun partTwo(input: List<String>): Long =
+    input.map { calcPart2(it.iterator()) }.sum()
+
+val operations: Map<Char, (Long, Long) -> Long> = mapOf(
+    '+' to { a, b -> (a + b) },
+    '*' to { a, b -> (a * b) }
+)
+
 private fun readInputData() =
     {}.javaClass.classLoader.getResourceAsStream("day18.txt")
         .reader()
-        .readLines()
+        .readLines().map {
+            it.replace(" ", "")
+        }
 
-fun partOne(input: List<String>): Int = 0
+fun calculateExpression(input: String): Long {
 
-fun partTwo(input: List<String>): Int = 0
+    var processedString = if (input.contains(")")) processParentheses(input)
+    else input
+
+    val numbers = processedString.split("+", "*").map(String::toLong)
+    val operators = processedString.filter { it in listOf('+', '*') }.iterator()
+
+    val startValue = numbers.first()
+    return numbers.drop(1).fold(
+        startValue
+    ) { acc, number ->
+        operations[operators.next()]?.invoke(acc, number) ?: acc
+    }
+}
+
+fun processParentheses(input: String, withPrecedence: Boolean = false): String {
+    if (!input.contains(")")) return input
+
+    val end = input.indexOf(")")
+    val begin = input.substring(0, end).lastIndexOf("(")
+    val expr = input.substring(begin + 1, end)
+    val res = calculateExpression(expr)
+    return processParentheses(input.substring(0, begin) + res + input.substring(end + 1), withPrecedence)
+}
+
+fun calcPart2(input: CharIterator): Long {
+    val multiplyList = mutableListOf<Long>()
+    var partialSum = 0L
+    while (input.hasNext()) {
+        val char = input.nextChar()
+        when {
+            char == '(' -> partialSum += calcPart2(input)
+            char == ')' -> break
+            char == '*' -> {
+                multiplyList += partialSum
+                partialSum = 0L
+            }
+            char.isDigit() -> partialSum += char.asLong()
+        }
+    }
+    return (multiplyList + partialSum).product()
+}
+
+fun Char.asLong(): Long =
+    this.toString().toLong()
+
+fun Iterable<Long>.product(): Long =
+    this.reduce { a, b -> a * b }
